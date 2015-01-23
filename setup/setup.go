4af167a6d024c154c9444063045abe4e16bd80be
8legd <git@8legd.com>
2015-01-23 19:31:46 +0000

    initial working version

--- setup/setup.go
+++ setup/setup.go
@@ -1,29 +1,41 @@
+// Package setup provides functions for setting up the logging directories
 package setup
 
-import "os"
-import "errors"
+import (
+  "os"
+  "path"
+  "errors"
+)
 
 func setupDir(dir string) error {
-  var err error
   f, err := os.Stat(dir)
-  if (err != nil) {
-    if (os.IsNotExist(err)) {
-      err = os.Mkdir(dir, 0777)
+  if err != nil {
+    if os.IsNotExist(err) {
+      return os.Mkdir(dir, 0777)
+    } else {
+      return err
     }
   } else {
     if !f.IsDir() {
-      err = errors.New(dir + " exists but is not a directory")
+      return errors.New(dir + " exists but is not a directory")
+    } else {
+      return nil
     }
   }
-  return err
 }
 
-
+// SetupLoggingDirs checks the specified directories exist and if not tries to create them
 func SetupLoggingDirs(baseDir string, logDirs []string) error {
-  var err error
-  err = setupDir(baseDir)
+  err := setupDir(baseDir)
   if err != nil {
-    // TODO loop through logDirs setting them up too
+    return err
+  } else {
+    for i := range logDirs {
+      err = setupDir(path.Join(baseDir,logDirs[i]))
+      if err != nil {
+        return err
+      }
+    }
+    return nil
   }
-  return err
 }
--- setup/setup_test.go
+++ setup/setup_test.go
@@ -10,5 +10,12 @@ func TestSetupLoggingDirs(t *testing.T) {
   if (err != nil) {
       t.Error(err)
   }
-  // TODO Tidy up (remove dirs)
+  // TODO
+  // Check dirs exist
+  // Test re-run of SetupLoggingDirs
+  // Check dirs still exist!
+  // Tidy up (remove dirs)
+  // Create a file with the same name as one of the directories
+  // Check this returns an appropriate error
+  // Tidy up (remove dirs)
 }
